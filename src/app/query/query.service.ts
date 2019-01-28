d9494ac860cad6a5f993dc5fc9cf0662a68f9ba2
petr <stechy12@gmail.com>
2019-01-28 17:25:31 +0000

    #1 Implementace průvodce vytvořením nového dotazu-

--- src/app/query/query.service.ts
+++ src/app/query/query.service.ts
@@ -3,11 +3,11 @@ import { Query } from './query';
 import { LocalStorageService } from 'angular-2-local-storage';
 
 interface QueryStorageEntry {
-  id: string;
-  name: string;
-  description: string;
-  endpoint: string;
-  content: string;
+  _id: string;
+  _name: string;
+  _description: string;
+  _endpoint: string;
+  _content: string;
 }
 
 @Injectable({
@@ -20,26 +20,30 @@ export class QueryService {
   private _queries = new Array<Query>();
 
   constructor(private storage: LocalStorageService) {
-    storage.set(QueryService.STORAGE_KEY, JSON.stringify([
-      {
-        'id': '_y4379asvp',
-        'name': 'Skript 1',
-        'description': 'Popis 1. skriptu',
-        'endpoint': '/ds1/query',
-        'content': 'SELECT ?subject ?predicate ?object\nWHERE {\n  ?subject ?predicate ?object\n}\nLIMIT 25'
-      },
-      {
-        'id': '_giaolge8p',
-        'name': 'Skript 2',
-        'description': 'Popis 2. skriptu',
-        'endpoint': '/ds2/query',
-        'content': 'SELECT ?subject ?predicate ?object\nWHERE {\n  ?subject ?predicate ?object\n}\nLIMIT 25'
-      }]));
+    // storage.set(QueryService.STORAGE_KEY, JSON.stringify([
+    //   {
+    //     'id': '_y4379asvp',
+    //     'name': 'Skript 1',
+    //     'description': 'Popis 1. skriptu',
+    //     'endpoint': '/ds1/query',
+    //     'content': 'SELECT ?subject ?predicate ?object\nWHERE {\n  ?subject ?predicate ?object\n}\nLIMIT 25'
+    //   },
+    //   {
+    //     'id': '_giaolge8p',
+    //     'name': 'Skript 2',
+    //     'description': 'Popis 2. skriptu',
+    //     'endpoint': '/ds2/query',
+    //     'content': 'SELECT ?subject ?predicate ?object\nWHERE {\n  ?subject ?predicate ?object\n}\nLIMIT 25'
+    //   }]));
     this._loadQueries();
   }
 
   private static parseQuery(input: QueryStorageEntry): Query {
-    return new Query(input.id, input.name, input.endpoint, input.content, input.description);
+    return new Query(input._id, input._name, input._endpoint, input._content, input._description);
+  }
+
+  private static makeID(): string {
+    return '_' + Math.random().toString(36).substr(2, 9);
   }
 
   /**
@@ -76,5 +80,22 @@ export class QueryService {
    */
   byId(id: string): Query {
     return this._queries.find(query => id === query.id);
+  }
+
+  /**
+   * Vytvoří záznam o novém dotazu
+   *
+   * @param name Název dotazu
+   * @param endpoint Endpoint, na který se bude dotaz posílat
+   * @param description Popis dotazu
+   * @param tags Seznam tagů, které odpovídají danému dotazu
+   * @param content Obsah samotného dotazu
+   * @param variables Proměnné, které se vyskytují v dotazu
+   */
+  create(name: string, endpoint: string, description: string, tags: string[], content: string, variables: {}) {
+    description = description || 'Prázdný popis';
+    const query = new Query(QueryService.makeID(), name, endpoint, content, description);
+    this._queries.push(query);
+    this._saveQueries();
   }
 }
