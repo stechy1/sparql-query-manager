50f2a117aa1802b41b5acf8751ac72b76f85e6a0
petr <stechy12@gmail.com>
2019-05-10 17:55:25 +0100

    Upravení pipy pro zobrazení délky požadavku na server tak, aby respektovala nastavení.

--- src/app/time.pipe.ts
+++ src/app/time.pipe.ts
@@ -1,12 +1,42 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+export interface TimeFormat {
+  showHours: boolean;
+  showMinutes: boolean;
+  showSeconds: boolean;
+  showMiliseconds: boolean;
+}
+
 @Pipe({
   name: 'time'
 })
 export class TimePipe implements PipeTransform {
 
-  transform(value: number, args?: any): any {
+  transform(value: number, format: TimeFormat): any {
     const date = new Date(value);
-    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
+    let result = '';
+
+    if (format.showHours) {
+      result += `${date.getHours()}h `;
+    }
+    if (format.showMinutes) {
+      result += `${date.getMinutes()}m `;
+    }
+    if (format.showSeconds) {
+      result += `${date.getSeconds()}s `;
+    }
+    if (format.showMiliseconds) {
+      result += `${date.getMilliseconds()}ms`;
+    }
+
+    if (!result || result === '' || result.length === 0) {
+      return '';
+    }
+
+    if (result.charAt(result.length - 1) === ' ') {
+      result = result.substr(0, result.length - 1);
+    }
+
+    return result;
   }
 }
