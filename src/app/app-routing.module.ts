032022713ed4996d14fd1e2e55818ee530014b68
petr <stechy12@gmail.com>
2019-06-07 17:57:03 +0100

    První část updatu angularu na verzi 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: 'edit', loadChildren: './edit-query/edit-query.module#EditQueryModule'},
-  {path: 'result-viewer', loadChildren: './result-viewer/result-viewer.module#ResultViewerModule'},
-  {path: 'browse-results', loadChildren: './browse-results/browse-results.module#BrowseResultsModule'},
-  {path: 'settings', loadChildren: './settings/settings.module#SettingsModule'},
+  {path: 'edit', loadChildren: () => import('./edit-query/edit-query.module').then(m => m.EditQueryModule)},
+  {path: 'result-viewer', loadChildren: () => import('./result-viewer/result-viewer.module').then(m => m.ResultViewerModule)},
+  {path: 'browse-results', loadChildren: () => import('./browse-results/browse-results.module').then(m => m.BrowseResultsModule)},
+  {path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule)},
   {path: '', pathMatch: 'full', redirectTo: 'browse-query'}
 ];
 
