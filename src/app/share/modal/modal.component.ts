1e52126545478900dee3cb0c49abb29621b00037
petr <stechy12@gmail.com>
2019-06-09 22:28:58 +0100

    #42 Přidání komentárů.

--- src/app/share/modal/modal.component.ts
+++ src/app/share/modal/modal.component.ts
@@ -4,6 +4,36 @@ import { ModalService } from './modal.service';
 import { animation } from './modal.animation';
 import { Observable, Subscription } from 'rxjs';
 
+/**
+ * Komponenta reprezentující modální dialog
+ * Dialog disponuje dvěma tlačítky: <b>zrušit</b>, <b>potvrdit</b>.
+ *
+ * Pracuje ve dvou módech:
+ *  - read-only - pouze pro zobrazení informace
+ *  - s čekáním na výsledek - předpokládá se nějaká interakce v dialogu s výsledkem
+ *
+ * V read-only režimu je vhodné dialog použít takto:
+ *  <pre>
+ *    <app-modal id="informationModalDialog" title="Informace"
+ *      (confirm)="handleConfirmInformationDialog()"
+ *      (cancel)="handleCnacelConfirmationDialog()">
+ *      <app-import-dialog></app-import-dialog>
+ *    </app-modal>
+ *  </pre>
+ *
+ *  Pro čekání na výsledek je potřeba spárovat proměnnou 'result':
+ *  <pre>
+ *    <app-modal id="modalContainer" title="Import dotazů"
+ *      [result]="importDialog.entries"
+ *      (confirm)="importDialog.doImport()">
+ *      <app-import-dialog #importDialog></app-import-dialog>
+ *    </app-modal>
+ *  </pre>
+ *
+ * V druhém případě se výsledek musí získat právě z komponenty uvnitř dialogu,
+ * proto se nabinduje metoda 'confirm', která informuje komponentu v dialogu,
+ * že má poskytnout výsledek do 'spárované' proměnné.
+ */
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
@@ -14,15 +44,24 @@ import { Observable, Subscription } from 'rxjs';
 })
 export class ModalComponent implements OnInit, OnDestroy {
 
+  // ID dialogu
   @Input() id: string;
+  // Titulek dialogu
   @Input() title: string;
+  // Pozorovatelný výsledek, který se použije v metodě openForResult
   @Input() result: Observable<any>;
+  // Zrušení akce v dialogu
   @Output() cancel = new EventEmitter<void>();
+  // Potvrzení akce v dialogu
   @Output() confirm = new EventEmitter<void>();
 
+  // Reference na komponentu dialogu
   private readonly element: any;
+  // Příznak, který říká, zda-li je dialog otevřený
   private _isOpen = false;
+  // Pomocná reference na odběr výsledku
   private _resultSubscription: Subscription;
+  // Pomocná reference na odběr akce zrušení dialogu
   private _cancelSubscription: Subscription;
 
   constructor(private _modalService: ModalService, private el: ElementRef) {
@@ -30,25 +69,38 @@ export class ModalComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    // Ujistím se, že dialog má nastavené ID
     if (!this.id) {
       console.error('modal must have an id');
       return;
     }
 
+    // Vložím dialog až na samý konec DOMu
     document.body.appendChild(this.element);
+    // Přidám dialog do správce dialogů
     this._modalService.add(this);
   }
 
   ngOnDestroy(): void {
+    // Dialog již není používán, tak ho odeberu ze správce dialogů
     this._modalService.remove(this.id);
+    // Teď ho odeberu z DOMu
     this.element.remove();
   }
 
+  /**
+   * Otevře dialog bez čekání na výsledek
+   */
   open(): void {
+    // Nastaví příznak na otevřeno
     this._isOpen = true;
+    // Přidá třídu 'modal-open' do elementu 'body'
     document.body.classList.add('modal-open');
   }
 
+  /**
+   * Otevře dialog s čekáním na výsledek
+   */
   openForResult(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
       this.open();
@@ -63,6 +115,9 @@ export class ModalComponent implements OnInit, OnDestroy {
     });
   }
 
+  /**
+   * Zavře dialog
+   */
   close(): void {
     this._isOpen = false;
     document.body.classList.remove('modal-open');
--- src/app/share/modal/modal.service.ts
+++ src/app/share/modal/modal.service.ts
@@ -4,8 +4,14 @@ import { ModalComponent } from './modal.component';
 @Injectable({ providedIn: 'root' })
 export class ModalService {
 
+  // Kolekce modálních dialogů
   private _modals: ModalComponent[] = [];
 
+  /**
+   * Přidá nový modální dialog do správce
+   *
+   * @param modal Nový modální dialog
+   */
   add(modal: ModalComponent) {
     if (modal === undefined) {
       return;
@@ -14,20 +20,40 @@ export class ModalService {
     this._modals.push(modal);
   }
 
+  /**
+   * Odstraní modální dialog podle jeho ID
+   *
+   * @param id ID dialogu, který má být odstraněn
+   */
   remove(id: string) {
     this._modals = this._modals.filter(x => x.id !== id);
   }
 
+  /**
+   * Otevře dialog v read-only režimu
+   *
+   * @param id ID dialogu, který se má otevřít
+   */
   open(id: string) {
     const modal: ModalComponent = this._modals.filter(x => x.id === id)[0];
     modal.open();
   }
 
+  /**
+   * Otevře dialog s čekáním na výsledek
+   *
+   * @param id ID dialogu, který se má otevřít
+   */
   openForResult(id: string): Promise<any> {
     const modal: ModalComponent = this._modals.filter(x => x.id === id)[0];
     return modal.openForResult();
   }
 
+  /**
+   * Zavře vybraný dialog
+   *
+   * @param id ID dialogu, který se má zavřít
+   */
   close(id: string) {
     const modal: ModalComponent = this._modals.filter(x => x.id === id)[0];
     modal.close();
