15cdf80d4a908f5b7110394e0a7e52c9a142155e
petr <stechy12@gmail.com>
2019-06-12 18:49:35 +0100

    Odstranění mezery.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-
 const routes: Routes = [
   {path: 'edit', loadChildren: () => import('./edit-query/edit-query.module').then(m => m.EditQueryModule)},
   {path: 'result-viewer', loadChildren: () => import('./result-viewer/result-viewer.module').then(m => m.ResultViewerModule)},
