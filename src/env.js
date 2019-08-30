2af4211167be0e5fa5149d1abd5c7d6e6595770f
petr <stechy12@gmail.com>
2019-08-30 19:07:56 +0200

    Ukázka výchozího nastavení aplikace.

--- src/env.js
+++ src/env.js
@@ -2,7 +2,39 @@
   window.__env = window.__env || {};
 
   window.__env.defaultUserSettings = undefined;
+  // window.__env.defaultUserSettings = {
+  //   suffix: ' - (Duplicated)',
+  //   serverTimeout: 5000,
+  //   useSaveDelay: true,
+  //   saveDelay: 250,
+  //   queryResultTimeFormat: {
+  //     showHours: false,
+  //     showMinutes: false,
+  //     showSeconds: true,
+  //     showMiliseconds: true
+  //   },
+  //   queryParameterFormat: {
+  //     prefix: '$',
+  //     suffixIsPrefix: true,
+  //     suffix: '$'
+  //   },
+  //   defaultExportFileName: 'queries.json',
+  //   useGestures: true,
+  //   alwaysShowImportDialog: false,
+  //   corsHack: true,
+  //   corsURL: 'https://cors-anywhere.herokuapp.com'
+  // };
+
   // Firebase settings
   window.__env.firebase = undefined;
+  // window.__env.firebase = {
+  //   apiKey: 'here-put-your-firebase-api-key',
+  //   authDomain: 'here-put-your-application-url',
+  //   databaseURL: 'here-put-your-firebase-url',
+  //   projectId: 'project-name',
+  //   storageBucket: 'storage-bucket-url',
+  //   messagingSenderId: 'unique-id',
+  //   appId: 'application-id'
+  // };
 
 }(this));
